537ed32604188f44268550e58671a22085e6a051
Rafael Costa <rafael.txct@gmail.com>
2023-05-04 23:31:16 -0300

    sketching chat component

--- front/src/components/Chat.jsx
+++ front/src/components/Chat.jsx
@@ -5,30 +5,95 @@ function Chat() {
     <div className={styles.root}>
       <div className={styles.contactsDiv}>
         <nav className={styles.contactsNavbar}>
-          <p>asdfasdfasdf</p>
-          <p>asdfasdfasdf</p>
+          <div className={styles.contactsInfo}>
+            <p>Chats</p>
+            <div className={styles.contactsInfoOptions}>
+              <p>New</p>
+              <p>...</p>
+            </div>
+          </div>
+          <div className={styles.contactsSearchInput}>
+            <input type="text" placeholder="Filter" />
+          </div>
         </nav>
+        {/* CONTACTS EXAMPLE */}
         <div className={styles.contacts}>
-          <p>asdfasdfasdf</p>
-          <p>asdfasdfasdf</p>
-          <p>asdfasdfasdf</p>
-          <p>asdfasdfasdf</p>
+          <div className={styles.contact}>
+            <p className={styles.contactIcon}>icon</p>
+            <div className={styles.contactUnit}>
+              <div className={styles.contactName}>
+                <p>user</p>
+                <p>10:00</p>
+              </div>
+              <p className={styles.contactMessage}>message</p>
+            </div>
+          </div>
+          <div className={styles.contact}>
+            <p className={styles.contactIcon}>icon</p>
+            <div className={styles.contactUnit}>
+              <div className={styles.contactName}>
+                <p>user</p>
+                <p>10:00</p>
+              </div>
+              <p className={styles.contactMessage}>message</p>
+            </div>
+          </div>
+          <div className={styles.contact}>
+            <p className={styles.contactIcon}>icon</p>
+            <div className={styles.contactUnit}>
+              <div className={styles.contactName}>
+                <p>user</p>
+                <p>10:00</p>
+              </div>
+              <p className={styles.contactMessage}>message</p>
+            </div>
+          </div>
+          <div className={styles.contact}>
+            <p className={styles.contactIcon}>icon</p>
+            <div className={styles.contactUnit}>
+              <div className={styles.contactName}>
+                <p>user</p>
+                <p>10:00</p>
+              </div>
+              <p className={styles.contactMessage}>message</p>
+            </div>
+          </div>
+          {/* CONTACTS EXAMPLE */}
         </div>
       </div>
       <div className={styles.messagesDiv}>
         <nav className={styles.messagesNavbar}>
-          <p>asdfasdfasdf</p>
-          <p>asdfasdfasdf</p>
+          <div className={styles.messagesInfo}>
+            <p>Icon</p>
+            <p>Name</p>
+          </div>
+          <div className={styles.messagesOptions}>
+            <p>New</p>
+            <p>...</p>
+          </div>
         </nav>
         <div className={styles.messages}>
-          <p>asdfasdfasdf</p>
-          <p>asdfasdfasdf</p>
-          <p>asdfasdfasdf</p>
-          <p>asdfasdfasdf</p>
+          <div className={styles.messageSent}>
+            <p>asdfasdfasdf</p>
+          </div>
+          <div className={styles.messageReceived}>
+            <p>asdfasdfasdf</p>
+          </div>
+          <div className={styles.messageSent}>
+            <p>asdfasdfasdf</p>
+          </div>
+          <div className={styles.messageSent}>
+            <p>asdfasdfasdf</p>
+          </div>
+          <div className={styles.messageReceived}>
+            <p>asdfasdfasdf</p>
+          </div>
         </div>
         <div className={styles.messagesInput}>
-          <p>asdfasdfasdf</p>
-          <p>asdfasdfasdf</p>
+          <p>Emoji</p>
+          <p>Add</p>
+          <input type="text" placeholder="Message" />
+          <p>Voice</p>
         </div>
       </div>
     </div>
