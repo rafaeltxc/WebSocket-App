f88bdfb021488b4bcd29f72be36d22c830536fd2
Rafael Costa <rafael.txct@gmail.com>
2023-06-17 11:34:40 -0300

    Styling front

--- front/src/screens/Chat.jsx
+++ front/src/screens/Chat.jsx
@@ -2,6 +2,9 @@ import styles from '../styles/Chat.module.css';
 import RateReviewOutlinedIcon from '@mui/icons-material/RateReviewOutlined';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import MicIcon from '@mui/icons-material/Mic';
 
 import userDefault from '../images/user-default-profile.png';
 
@@ -13,8 +16,12 @@ function Chat() {
           <div className={styles.contactsInfo}>
             <p>Chats</p>
             <div className={styles.contactsInfoOptions}>
-              <RateReviewOutlinedIcon />
-              <MoreVertOutlinedIcon />
+              <button className={styles.button}>
+                <RateReviewOutlinedIcon />
+              </button>
+              <button className={styles.button}>
+                <MoreVertOutlinedIcon />
+              </button>
             </div>
           </div>
           <div className={styles.contactsSearchInput}>
@@ -22,9 +29,9 @@ function Chat() {
             <input type="text" placeholder="Filter" />
           </div>
         </nav>
-        {/* CONTACTS EXAMPLE */}
         <div className={styles.contacts}>
-          <div className={styles.contact}>
+          {/* CONTACTS EXAMPLE */}
+          <button className={styles.contact}>
             <img
               className={styles.contactIcon}
               src={userDefault}
@@ -37,8 +44,8 @@ function Chat() {
               </div>
               <p className={styles.contactMessage}>message</p>
             </div>
-          </div>
-          <div className={styles.contact}>
+          </button>
+          <button className={styles.contact}>
             <img
               className={styles.contactIcon}
               src={userDefault}
@@ -46,258 +53,101 @@ function Chat() {
             />
             <div className={styles.contactUnit}>
               <div className={styles.contactName}>
-                <p>user</p>
+                <p className={styles.contactUsername}>user</p>
                 <p>10:00</p>
               </div>
               <p className={styles.contactMessage}>message</p>
             </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
-          <div className={styles.contact}>
-            <img
-              className={styles.contactIcon}
-              src={userDefault}
-              alt="User profile picture"
-            />
-            <div className={styles.contactUnit}>
-              <div className={styles.contactName}>
-                <p>user</p>
-                <p>10:00</p>
-              </div>
-              <p className={styles.contactMessage}>message</p>
-            </div>
-          </div>
+          </button>
           {/* CONTACTS EXAMPLE */}
         </div>
       </div>
       <div className={styles.messagesDiv}>
         <nav className={styles.messagesNavbar}>
           <div className={styles.messagesInfo}>
-            <p>Icon</p>
-            <p>Name</p>
+            <img
+              className={styles.contactIcon}
+              src={userDefault}
+              alt="User profile picture"
+            />
+            <p className={styles.contactUsername}>user</p>
           </div>
           <div className={styles.messagesOptions}>
-            <p>New</p>
-            <p>...</p>
+            <button className={styles.button}>
+              <MoreVertOutlinedIcon />
+            </button>
           </div>
         </nav>
         <div className={styles.messages}>
+          {/* // Messages Examples */}
           <div className={styles.messageSent}>
-            <p>asdfasdfasdf</p>
+            <div className={styles.messageSentP}>
+              <p>
+                asdfasdfaasdasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdasdasdasdsdf
+              </p>
+            </div>
+          </div>
+          <div className={styles.messageSent}>
+            <div className={styles.messageSentP}>
+              <p>asdfasdfasdfdfasdf</p>
+            </div>
           </div>
           <div className={styles.messageReceived}>
-            <p>asdfasdfasdf</p>
+            <div className={styles.messageReceivedP}>
+              <p>asdfasdfasdsadffasdfasdfasdfasdfafsdfasdfasasdfasdfasdff</p>
+            </div>
           </div>
           <div className={styles.messageSent}>
-            <p>asdfasdfasdf</p>
-          </div>
-          <div className={styles.messageSent}>
-            <p>asdfasdfasdf</p>
+            <div className={styles.messageSentP}>
+              <p>asdfasdfasdf</p>
+            </div>
           </div>
           <div className={styles.messageReceived}>
-            <p>asdfasdfasdf</p>
+            <div className={styles.messageReceivedP}>
+              <p>asdfasdfasdf</p>
+            </div>
           </div>
+          <div className={styles.messageSent}>
+            <div className={styles.messageSentP}>
+              <p>
+                asdfasdfaasdasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasdasdasdasdasdsdf
+              </p>
+            </div>
+          </div>
+          <div className={styles.messageSent}>
+            <div className={styles.messageSentP}>
+              <p>asdfasdfasdfdfasdf</p>
+            </div>
+          </div>
+          <div className={styles.messageReceived}>
+            <div className={styles.messageReceivedP}>
+              <p>asdfasdfasdsadffasdfasdfasdfasdfafsdfasdfasasdfasdfasdff</p>
+            </div>
+          </div>
+          <div className={styles.messageSent}>
+            <div className={styles.messageSentP}>
+              <p>asdfasdfasdf</p>
+            </div>
+          </div>
+          <div className={styles.messageReceived}>
+            <div className={styles.messageReceivedP}>
+              <p>asdfasdfasdf</p>
+            </div>
+          </div>
+          {/* // Messages Examples */}
         </div>
         <div className={styles.messagesInput}>
-          <p>Emoji</p>
-          <p>Add</p>
+          <button className={styles.button}>
+            <SentimentSatisfiedAltIcon />
+          </button>
+          <button className={styles.button}>
+            <AttachFileIcon />
+          </button>
+
           <input type="text" placeholder="Message" />
-          <p>Voice</p>
+          <button className={styles.button}>
+            <MicIcon />
+          </button>
         </div>
       </div>
     </div>
